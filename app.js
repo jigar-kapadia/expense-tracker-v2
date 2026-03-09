f5639e1c9e943e2a3c809286533875df76acc8f1
Jigar Kapadia <jigark00@gmail.com>
2026-03-09 23:35:41 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const SUPABASE_URL = "YOUR_SUPABASE_URL";
+const SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY";
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
+setTodayDate();
+loadExpenses();
+
+function setTodayDate() {
+    const today = new Date().toISOString().split("T")[0];
+    document.getElementById("date").value = today;
+}
+
+async function addExpense() {
+
+    const date = document.getElementById("date").value;
+    const category = document.getElementById("category").value;
+    const amount = document.getElementById("amount").value;
+    const notes = document.getElementById("notes").value;
+
+    const error = document.getElementById("error");
+    error.innerText = "";
+
+    if (!category) {
+        error.innerText = "Category required";
+        return;
+    }
+
+    if (!amount || amount <= 0) {
+        error.innerText = "Enter valid amount";
+        return;
+    }
+
+
+    console.log({ date, category, amount, notes });
+
+    // await client
+    //     .from("expenses")
+    //     .insert({
+    //         date: date,
+    //         category: category,
+    //         amount: amount,
+    //         notes: notes
+    //     });
+
+    // clearForm();
+    // loadExpenses();
+}
+
+function clearForm() {
+    document.getElementById("amount").value = "";
+    document.getElementById("notes").value = "";
+    document.getElementById("category").value = "";
+}
+
+async function loadExpenses() {
+
+    let query = client.from("expenses").select("*");
+
+    const start = document.getElementById("startDate").value;
+    const end = document.getElementById("endDate").value;
+    const category = document.getElementById("filterCategory").value;
+
+    if (start) query = query.gte("date", start);
+    if (end) query = query.lte("date", end);
+    if (category) query = query.eq("category", category);
+
+    const { data } = await query.order("date", { ascending: false });
+
+    renderExpenses(data);
+}
+
+function renderExpenses(data) {
+
+    const container = document.getElementById("expenseList");
+
+    container.innerHTML = "";
+
+    let total = 0;
+
+    data.forEach(e => {
+
+        total += Number(e.amount);
+
+        container.innerHTML += `
+<div class="card">
+<div class="card-top">
+<span>${e.category}</span>
+<span>₹${e.amount}</span>
+</div>
+<div class="card-bottom">
+${e.date} • ${e.notes || ""}
+</div>
+</div>
+`;
+
+    });
+
+    document.getElementById("totalAmount").innerText = total;
+
+}
