977b6e03d794c3a80733f303e89150513331ea67
Jigar Kapadia <jigark00@gmail.com>
2026-03-10 01:07:35 +0530

    added config js

---
+++ config.js
@@ -0,0 +1,4 @@
+window.env = {
+    SUPABASE_URL: "https://ipgagfjsftyiorhbebhp.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlwZ2FnZmpzZnR5aW9yaGJlYmhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMwNjQwODcsImV4cCI6MjA4ODY0MDA4N30.Rx5iLu6a0uRO3N8d3WaHMKFwY4N2TGu-29zhDKZYg1s"
+};
